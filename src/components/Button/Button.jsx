9e01da64802bb76d1163c7e1c8e1f5dfadf5c69c
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-10-06 11:35:33 +0300

    Update Button.jsx
    Add styles to the button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 export default function ButtonUsage({ text, onClick }) {
+  const StylesButton = styled(Button)(({ theme }) => ({
+    width: '274px',
+    padding: '12px 0 12px 0',
+    borderRadius: '12px',
+    color: theme.palette.getContrastText('#0B44CD'),
+    backgroundColor: '#0B44CD',
+    '&:hover': {
+      backgroundColor: '#3470FF',
+    },
+  }));
+
   return (
-    <Button variant="contained" onClick={onClick}>
+    <StylesButton variant="contained" onClick={onClick}>
       {text}
-    </Button>
+    </StylesButton>
   );
 }
