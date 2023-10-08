286478774429666490df5f1073474c0d76423983
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-10-08 13:12:29 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <HeaderWrapper>
       <Container>
-        <NavMenu to={'/'}>{'Home'}</NavMenu>
-        <NavMenu to={'/catalog'}>{'Catalog'}</NavMenu>
-        <NavMenu to={'/favorites'}>{'Favorites'}</NavMenu>
+        <NavMenu to="/">Home</NavMenu>
+        <NavMenu to="/catalog">Catalog</NavMenu>
+        <NavMenu to="/favorites">Favorites</NavMenu>
       </Container>
     </HeaderWrapper>
   );
