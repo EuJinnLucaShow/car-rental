2ea6ab90b42fdee6f114ebc70939b99b9a2c8af9
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-10-09 20:23:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 
 import Layout from './components/Layout/Layout';
-import { Loader } from 'components/Loader/Loader';
 
 const Home = lazy(() => import('pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('pages/CatalogPage/CatalogPage'));
@@ -10,16 +9,14 @@ const Favorites = lazy(() => import('pages/FavoritePage/FavoritePage'));
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="favorites" element={<Favorites />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 }
 
