32dbe23fb8c08ad96959c4943a4593617d7e3466
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-10-23 12:43:25 +0300

    Update FavoriteIcon.jsx

--- src/components/FavoriteIcon/FavoriteIcon.jsx
+++ src/components/FavoriteIcon/FavoriteIcon.jsx
@@ -3,7 +3,6 @@ import { StylesFavoriteBorder, StylesFavorite } from './FavoriteIcon.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFavorite, removeFavorite } from '../../redux/favoriteSlice';
 import { selectFavorites } from 'redux/selectors';
-import { useEffect } from 'react';
 
 export default function FavoriteIcon({ data }) {
   const dispatch = useDispatch();
