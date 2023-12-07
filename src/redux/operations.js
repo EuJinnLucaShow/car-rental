01cf8f7a1661cef89774852d2f66dfe057a2be48
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-07 09:02:29 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://6523a229ea560a22a4e894dd.mockapi.io/',
+    baseUrl: 'https://65634c50ee04015769a705d6.mockapi.io/',
   }),
   tagTypes: ['Advert'],
   endpoints: builder => ({
